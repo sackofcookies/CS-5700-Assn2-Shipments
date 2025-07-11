008a66bc96bb61a00aeb200d18c658268f3056b6
sackofcookies <tommy.lesueur.temp@outlook.com>
2025-07-10 19:12:26 -0600

    done for the day still getting some things working

--- composeApp/src/desktopMain/kotlin/org/interface/TrackerViewHelper.kt
+++ composeApp/src/desktopMain/kotlin/org/interface/TrackerViewHelper.kt
@@ -9,13 +9,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
+private val format = SimpleDateFormat("HH:mm:ss dd/MM/yyyy")
 
 class TrackerViewHelper(shipment: Shipment): ShipmnetObserver{
     val shipmentId: String = shipment.id
     val shipmentNotes = remember{mutableStateListOf<String>()}
     val shipmentUpdateHistory = remember{mutableStateListOf<StatusChange>()}
-    var expectedShipmentDeliveryDate = remember{mutableStateOf<>(shipment.expectedDeliveryDateTimestamp)}
+    var expectedShipmentDeliveryDate = remember{mutableStateOf<Long>(shipment.expectedDeliveryDateTimestamp)}
     var shipmentStatus = remember{mutableStateOf<String>(shipment.status)}
     init{
         shipment.notes.forEach {shipmentNotes.add(it)}
@@ -29,5 +36,14 @@ class TrackerViewHelper(shipment: Shipment): ShipmnetObserver{
         shipment.updateHistory.forEach {shipmentUpdateHistory.add(it)}
         expectedShipmentDeliveryDate.value = shipment.expectedDeliveryDateTimestamp
         shipmentStatus.value = shipment.status
+    }
+
+    @Composable
+    public fun compose(){
+        Column {
+            Text(shipmentId)
+            Text("Status: " + shipmentStatus)
+            Text("Expected Delivery Date: " + java.util.Date(expectedShipmentDeliveryDate))
+        }
     }
 }
