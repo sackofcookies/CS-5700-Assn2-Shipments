028fabc2ee4ccb0538d3e8051177abfbba9d9206
sackofcookies <tommy.lesueur.temp@outlook.com>
2025-07-12 23:33:05 -0600

    helper tests

--- composeApp/src/desktopTest/kotlin/org/util/SimulatorTest.kt
+++ composeApp/src/desktopTest/kotlin/org/util/SimulatorTest.kt
@@ -77,4 +77,33 @@ class TrackingSimulatorTest {
             file.delete()
         }
     }
+
+    private fun runSimulatorWithLine(line: String): Shipment? = runBlocking {
+        val tmpFile = createTempFile().toFile()
+        try {
+            tmpFile.writeText(
+                """
+                created,shipment1
+                $line
+                """.trimIndent()
+            )
+            val simulator = TrackingSimulator()
+            simulator.runSimulation(tmpFile)
+            return@runBlocking simulator.findShipment("shipment1")
+        } finally {
+            tmpFile.delete()
+        }
+    }
+
+    @Test
+    fun testDeliveredStatusUpdate() {
+        val shipment = runSimulatorWithLine("delivered,shipment1,1000")
+        assertNotNull(shipment)
+        assertEquals("delivered", shipment.status)
+        assertEquals(1, shipment.updateHistory.size)
+        val update = shipment.updateHistory.first()
+        assertEquals("created", update.previousStatus)
+        assertEquals("delivered", update.newStatus)
+        assertEquals(1000L, update.timeStamp)
+    }
 }
